bb63ef4e0921095ce3d37acd2f2bc3481987d610
wiktorstyczen <56415287+wiktorstyczen@users.noreply.github.com>
2023-11-02 09:17:19 +0100

    DEMO

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+  const loaderWrapper = document.querySelector('.loader-wrapper');
+  const contentWrapper = document.querySelector('.content-wrapper');
+
+  loaderWrapper.style.display = 'none';
+  contentWrapper.style.display = 'block';
+});
